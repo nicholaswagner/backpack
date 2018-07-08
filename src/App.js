096822df4e2b60aaa5c67a2de274384629252592
nicholaswagner <nicholas@secretsignal.com>
2018-07-08 15:08:41 -0500

    this is terrible.  i need to set up a vpn for initial testing

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const client_id = 24120;
+var code = new URLSearchParams(document.location.search).get('code');
+
+if (code) {
+  console.log(
+    'found a redirect code (assuming from bungo) -- attempting to get an auth token.'
+  );
+  let data = `client_id=${client_id}&grant_type=authorization_code&code=${code}`;
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener('readystatechange', function() {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+      debugger;
+    }
+  });
+
+  xhr.open('POST', 'https://www.bungie.net/platform/app/oauth/token/');
+  xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+  xhr.setRequestHeader('cache-control', 'no-cache');
+
+  xhr.send(data);
+} else {
+  let url = `https://www.bungie.net/en/OAuth/Authorize?client_id=${client_id}&response_type=code`;
+  window.location.href = url;
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">spin spin spin</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Authorizing in progress.</p>
       </div>
     );
   }
